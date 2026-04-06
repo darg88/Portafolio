95781f06c71c184ac73b2c47af4939198c87694e
ONINGRAD89 <144836554+ONINGRAD89@users.noreply.github.com>
2026-04-06 10:43:20 -0500

    Agregado banner flotante de En Desarrollo (WIP)

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // Reemplaza 'Portafolio' si tu repositorio en GitHub se llama diferente.
+  // Es muy importante que lleve las barras diagonales al inicio y al final.
+  base: '/Portafolio/', 
+})
